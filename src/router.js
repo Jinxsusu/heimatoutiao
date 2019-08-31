ed4beaeb569ccb6c1a7d1268e8baf372a5a9376c
Jinx <Jinx@qq.com>
2019-08-31 11:22:07 +0800

    传入静态资源

--- src/router.js
+++ src/router.js
@@ -1,23 +1,28 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Home from './views/home.vue'
+// views一般来说是放置路由级组件,就是直接挂到路由表上的components的组件,相当于我们原来的一个个页面
+import Login from './views/login'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+  routes: [{
+    path: '/login',
+    component: Login
+  },
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  }
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    // }
   ]
 })
