81e1217d0889ee76eb340b69027590b38108e2ee
Jinx <Jinx@qq.com>
2019-08-31 16:25:07 +0800

    axios准备

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'
+import axios from 'axios'
 import 'element-ui/lib/theme-chalk/index.css'
 import './style/index.less'
-
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
+// 给Vue对象的原型属性赋值
+Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 new Vue({
