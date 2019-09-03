86a327bfac01469abe76c8eb1aa2d1b26684e5d5
Jinx <Jinx@qq.com>
2019-09-03 19:18:00 +0800

    素材列表-页面结构-tab

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -3,15 +3,22 @@
     <bread-crumb slot="header">
       <template slot="title">素材管理</template>
     </bread-crumb>
+    <el-tabs v-model="activeName" >
+      <el-tab-pane label="全部素材" name="all">全部素材</el-tab-pane>
+      <el-tab-pane label="收藏图片" name="collect">收藏图片</el-tab-pane>
+    </el-tabs>
   </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      activeName: 'all'// 设置tab标签默认选中all一栏
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
