2739f6e036ae47a9e0fa06cb49a5b2075e502a82
Jinx <Jinx@qq.com>
2019-09-04 22:44:17 +0800

    内容列表-页面结构-搜索筛选

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -5,22 +5,29 @@
     </bread-crumb>
     <el-form style="margin-left:40px">
       <el-form-item label="文章状态:">
-        <el-radio-group>
-          <el-radio>全部</el-radio>
-          <el-radio>草稿</el-radio>
-          <el-radio>待审核</el-radio>
-          <el-radio>审核通过</el-radio>
-          <el-radio>审核失败</el-radio>
+        <el-radio-group @change="changeCondition" v-model="searchForm.status">
+          <el-radio :label="5">全部</el-radio>
+          <el-radio :label="0">草稿</el-radio>
+          <el-radio :label="1">待审核</el-radio>
+          <el-radio :label="2">审核通过</el-radio>
+          <el-radio :label="3">审核失败</el-radio>
         </el-radio-group>
       </el-form-item>
       <el-form-item label="频道列表:">
-        <el-select></el-select>
+        <!-- 绑定频道数据 -->
+        <el-select @change="changeCondition" v-model="searchForm.channel_id">
+          <!-- 循环生成 -->
+          <!-- 显示的是label显示的item的name value是id(标识 是真实值) -->
+          <el-option v-for="item in channels" :key="item.id" :label="item.name" :value="item.id"></el-option>
+        </el-select>
       </el-form-item>
       <el-form-item label="时间选择:">
+        <!-- value-format 设置绑定日期的格式 -->
         <el-date-picker
-          v-model="value1"
-          type="datetimerange"
-          range-separator="至"
+          @change="changeCondition"
+          value-format="yyyy-MM-dd"
+          v-model="searchForm.dateRange"
+          type="daterange"
           start-placeholder="开始日期"
           end-placeholder="结束日期"
         ></el-date-picker>
@@ -58,27 +65,72 @@
     </div>
   </el-card>
 </template>
-
 <script>
 export default {
   data () {
     return {
       list: [],
-      defaultImg: require('../../assets/img/avatar.jpg') // 转成base64字符串的格式
+      defaultImg: require('../../assets/img/avatar.jpg'), // 转成base64字符串的格式
+      searchForm: {
+        // 定义一个表单数据对象
+        // 把这个对象的一项一项的内容绑定到每一项搜索框
+        status: 5, // 绑定文章状态 文章状态:全部的是空
+        // 但是设置为null的话 默认全部选不到
+        // 所以我们设置一个不存在的是值5 然后绑定到标签上
+        channel_id: null, //
+        dateRange: [] // 时间选择器 格式是数组格式 显示的时间不是北京时间
+      },
+      channels: [] // 频道列表的数据
     }
   },
   methods: {
-    getArticles () {
+    changeCondition () {
+      // 第一次获取文章内容的时候 params为空
+      // 当执行changeCondition方法的时候我们传入了刚刚定义好的params 然后又执行getArticles()
+      // 频道 状态 日期 都是绑定这个方法
+      // 定义params对象
+      let params = {
+        // 这个传的是搜索部分的现在的数据
+        // 当文章状态是全部的时候 我们设置为5 要让他重新改成null
+        status: this.searchForm.status === 5 ? null : this.searchForm.status,
+        channel_id: this.searchForm.channel_id,
+        begin_pubdate:
+          this.searchForm.dateRange.length > 0
+            ? this.searchForm.dateRange[0]
+            : null,
+        end_pubdate:
+          this.searchForm.dateRange.length > 1
+            ? this.searchForm.dateRange[1]
+            : null
+        // dateRange 是一个数组 它的第一项是begin_pubdate 下标为0 ,如果它没有操作的话就是空 ,如果设置了的话数组长度就要>0 用三元表达式表示如下
+      }
+      this.getArticles(params)
+    },
+    getArticles (params) {
+      // 第一次获取文章内容的时候 params为空
+      // 当执行changeCondition方法的时候我们传入了刚刚定义好的params 然后又执行getArticles()
       this.$axios({
-        url: '/articles'
+        url: '/articles',
+        params: params
       }).then(res => {
-        console.log(res)
+        // console.log(res)
         this.list = res.data.results
+      })
+    },
+    // 获取文章频道数据
+    getChannels () {
+      // 发送请求
+      this.$axios({
+        url: '/channels'
+      }).then(res => {
+        // console.log(res)
+        this.channels = res.data.channels
       })
     }
   },
   created () {
-    this.getArticles()
+    this.getArticles() // 获取
+    this.getChannels() // 获取频道数据
   },
   filters: {
     // 定义一个过滤器 改变文字状态的显示
