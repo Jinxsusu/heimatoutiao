87ee955b0897c98bb61cb25a8552767bc9fe33b5
Jinx <Jinx@qq.com>
2019-09-06 19:12:56 +0800

    封面功能组件的封装

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -19,12 +19,16 @@
       </el-form-item>
       <el-form-item label="封面" style="margin-top:100px">
         <!-- 封面类型 -1:自动，0-无图，1-1张，3-3张 -->
-        <el-radio-group v-model="formData.cover.type">
+        <el-radio-group @change='changeCoverTpye' v-model="formData.cover.type">
           <el-radio :label="1">单图</el-radio>
           <el-radio :label="3">三图</el-radio>
           <el-radio :label="0">无图</el-radio>
           <el-radio :label="-1">自动</el-radio>
         </el-radio-group>
+      </el-form-item>
+      <el-form-item>
+        <!-- 封面组件  给父亲绑定 -->
+        <cover-image :images="formData.cover.images"></cover-image>
       </el-form-item>
       <el-form-item label="频道" prop="channel_id">
         <el-select v-model="formData.channel_id">
@@ -50,8 +54,9 @@ export default {
         title: '',
         content: '',
         cover: {
-          type: 0, // 设置默认为无图
-          images: [] //
+          // 封面类型 -1:自动，0-无图，1-1张，3-3张
+          type: 0,
+          images: []
         },
         channel_id: null
       },
@@ -86,8 +91,9 @@ export default {
       this.$refs.publishForm.validate(isOk => {
         if (isOk) {
           // 只有校验成功后才会判断有没有新增还是修改
-          let { articleId } = this.$route.params // 获取id
+          let { articleId } = this.$route.params // 获取id 获取参数 $route 编程式导航 $router
           this.$axios({
+            // 因为传的参数是一样的 所有两个方法可以合并
             url: articleId ? `/articles/${articleId}` : '/articles',
             method: articleId ? 'put' : 'post',
             params: { draft }, // draft是草稿 设置为false 默认设置为不是草稿
@@ -98,6 +104,16 @@ export default {
           })
         }
       })
+    },
+    // 切换封面类型 根据当前类型
+    changeCoverTpye () {
+      if (this.formData.cover.type === 1) {
+        this.formData.cover.images = ['']// 有一张封面 待选择
+      } else if (this.formData.cover.type === 3) {
+        this.formData.cover.images = ['', '', ''] // 有三张封面待选择
+      } else {
+        this.formData.cover.images = []// 其他情况无图 默认为空
+      }
     },
     getChannels () {
       this.$axios({
